c36a13bd13f2136dc41b75d69310f39d381caed8
Barnakrum <krzysiu03@tlen.pl>
2023-12-08 17:40:04 +0100

    Moved declared and exported components to one array

--- src/app/modules/authentication/authentication.module.ts
+++ src/app/modules/authentication/authentication.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatFormFieldModule} from '@angular/material/form-field'
 import {MatInputModule} from '@angular/material/input';
+import { LoginComponent } from './login/login.component';
 
+
+const components = [LoginComponent]
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ...components
+  ],
   imports: [
     CommonModule,
     MatFormFieldModule,
     MatInputModule
+  ],
+  exports: [
+    ...components
   ]
 })
 export class AuthenticationModule { }
