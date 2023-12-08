534e942d9cce7c164aa3566ee22d481691ef569e
Barnakrum <krzysiu03@tlen.pl>
2023-12-08 18:56:13 +0100

    Added route for register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/home.component';
 import { LoginComponent } from './modules/authentication/login/login.component';
+import { RegisterComponent } from './modules/authentication/register/register.component';
 
 const routes: Routes = [
   {
@@ -16,6 +17,10 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
   },
 ];
 @NgModule({
