d6e4039120750bf869923a512208bd7adbaa782f
Barnakrum <krzysiu03@tlen.pl>
2023-12-08 23:10:59 +0100

    Added formGroup, Validators and onFormSubmit

--- src/app/modules/authentication/register/register.component.ts
+++ src/app/modules/authentication/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'gt-register',
@@ -6,7 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
-  errorMessage: string | undefined;
+  constructor(private formBuilder: FormBuilder) {}
 
-  ngOnInit(): void {}
+  formGroup: FormGroup | undefined;
+
+  errorMessage: string | undefined;
+  ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      email: [undefined, [Validators.required, Validators.email]],
+      password: [
+        undefined,
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{0,}$/gm),
+        ],
+      ],
+      repeatPassword: [undefined, Validators.required],
+    });
+  }
+
+  onFormSubmit(): void {
+    console.log(this.formGroup?.value);
+  }
 }
