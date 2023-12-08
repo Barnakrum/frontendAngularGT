67c7e9975d08a9ffe57add91a0610bfacf312247
Barnakrum <krzysiu03@tlen.pl>
2023-12-08 18:55:34 +0100

    Initial commit

---
+++ src/app/modules/authentication/form-switcher/form-switcher.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { ThemePalette } from '@angular/material/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'gt-form-switcher',
+  templateUrl: './form-switcher.component.html',
+  styleUrls: ['./form-switcher.component.css'],
+})
+export class FormSwitcherComponent implements OnInit {
+  links: string[] = ['login', 'register'];
+  activeLink: string | undefined;
+
+  background: ThemePalette = 'primary';
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.activeLink = this.router.url.replace('/', '');
+  }
+}
