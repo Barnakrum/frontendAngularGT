78f8b3a51473b14c8a76934a291e8429b0981a5b
Barnakrum <krzysiu03@tlen.pl>
2023-12-08 18:36:49 +0100

    Added Formswitcher and register input to components array

--- src/app/modules/authentication/authentication.module.ts
+++ src/app/modules/authentication/authentication.module.ts
@@ -1,24 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatFormFieldModule} from '@angular/material/form-field'
-import {MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { LoginComponent } from './login/login.component';
+import { FormSwitcherComponent } from './form-switcher/form-switcher.component';
+import { MatTabsModule } from '@angular/material/tabs';
+import { RegisterComponent } from './register/register.component';
 
-
-const components = [LoginComponent]
-
+const components = [LoginComponent, FormSwitcherComponent, RegisterComponent];
 
 @NgModule({
-  declarations: [
-    ...components
-  ],
-  imports: [
-    CommonModule,
-    MatFormFieldModule,
-    MatInputModule
-  ],
-  exports: [
-    ...components
-  ]
+  declarations: [...components],
+  imports: [CommonModule, MatFormFieldModule, MatInputModule, MatTabsModule],
+  exports: [...components],
 })
-export class AuthenticationModule { }
+export class AuthenticationModule {}
