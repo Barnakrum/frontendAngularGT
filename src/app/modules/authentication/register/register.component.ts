f2593da8e84361182079d643c299652d8c815ad3
Barnakrum <krzysiu03@tlen.pl>
2023-12-08 23:49:56 +0100

    Added registering functionality, no full validation yet

--- src/app/modules/authentication/register/register.component.ts
+++ src/app/modules/authentication/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { UserServiceService } from 'src/app/services/user-service.service';
 
 @Component({
   selector: 'gt-register',
@@ -7,7 +9,12 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserServiceService
+  ) {}
+
+  registerSubscription: Subscription | undefined;
 
   formGroup: FormGroup | undefined;
 
@@ -28,6 +35,14 @@ export class RegisterComponent implements OnInit {
   }
 
   onFormSubmit(): void {
-    console.log(this.formGroup?.value);
+    if (this.formGroup?.valid) {
+      this.registerSubscription = this.userService
+        .registerUser(this.formGroup?.value)
+        .subscribe();
+    }
+  }
+
+  ngOnDestory(): void {
+    this.registerSubscription?.unsubscribe();
   }
 }
